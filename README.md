525241cbaf0be09a10b89895cca9f1369c0fea3c
lilkimo <zarkokuljis.zk@gmail.com>
2020-11-25 16:37:50 -0300

    added function parser

---
+++ README.md
@@ -0,0 +1,8 @@
+# Integrantes
+Camilo Gonzalez, 201873550-7
+
+Zarko Kuljis, 201823523-7
+# Consideraciones
+* El nombre de cada función sólo será de 1 caracter alfabético.
+* Cada función recibirá sólo 1 parámetro.
+* El nombre del parámetro será de 1 sólo caracter alfabético.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Main {
+    static private Pattern equationParser = Pattern.compile(" *(?<function>[a-zA-Z])\\((?<argument>[a-zA-Z])\\) *= *(?<equation>(?:\\k<argument>|\\d|\\(|\\)|\\+|\\-|\\*|\\/)+) *");
     public static void main(String args[]) {
-        ;
+        Matcher matcher = equationParser.matcher("f(x)=2*x+3");
+        Boolean matchFound = matcher.find();
+        if (matchFound) {
+            System.out.println(matcher.group("function"));
+            System.out.println(matcher.group("argument"));
+            System.out.println(matcher.group("equation"));
+        }
+        else
+            System.out.println("0 matches");
     }
 }
