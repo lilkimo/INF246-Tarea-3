37c45436a94a7ae4ee9e85f4afe7f59be8ffb63f
lilkimo <zarkokuljis.zk@gmail.com>
2020-11-25 16:38:25 -0300

    added function parser

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Main {
-    static private Pattern equationParser = Pattern.compile(" *(?<function>[a-zA-Z])\\((?<argument>[a-zA-Z])\\) *= *(?<equation>(?:\\k<argument>|\\d|\\(|\\)|\\+|\\-|\\*|\\/)+) *");
+    static private Pattern functionParser = Pattern.compile(" *(?<name>[a-zA-Z])\\((?<argument>[a-zA-Z])\\) *= *(?<equation>(?:\\k<argument>|\\d|\\(|\\)|\\+|\\-|\\*|\\/)+) *");
     public static void main(String args[]) {
-        Matcher matcher = equationParser.matcher("f(x)=2*x+3");
+        Matcher matcher = functionParser.matcher("f(x)=2*x+3");
         Boolean matchFound = matcher.find();
         if (matchFound) {
-            System.out.println(matcher.group("function"));
+            System.out.println(matcher.group("name"));
             System.out.println(matcher.group("argument"));
             System.out.println(matcher.group("equation"));
         }
