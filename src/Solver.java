8913e0e2d305b3fe3f349a7755148c4f4310131c
lilkimo <zarkokuljis.zk@gmail.com>
2020-12-10 19:56:42 -0300

    fixed Solver.solve return

--- src/Solver.java
+++ src/Solver.java
@@ -5,51 +5,56 @@ import java.util.regex.Pattern;
 
 import sun.security.ec.point.ProjectivePoint.Mutable;
 
-
 public class Solver extends Thread {
     private MutableString solution;
     private String input;
     private Map<Character, Function> functions;
-    
-    public Solver(MutableString solution, String input, Map<Character, Function> functions){
+
+    public Solver(MutableString solution, String input, Map<Character, Function> functions) {
         this.solution = solution;
         this.input = input;
         this.functions = functions;
     }
 
-    public void run(){
+    public void run() {
         List<String> toResolve = Function.getFunctions(input);
-        
+
         Function functionToSolve;
         String functionEvaluated;
         String argument;
-        for (String function: toResolve) {
+        for (String function : toResolve) {
             functionToSolve = functions.get(function.charAt(0));
-            argument = function.substring(2, function.length()-1);
+            argument = function.substring(2, function.length() - 1);
             functionEvaluated = functionToSolve.equation.replaceAll(functionToSolve.parameter.toString(), argument);
-            
+
             solution.replaceAll(function, '(' + functionEvaluated + ')', true);
             // Crear una nueva hebra con input = functionEvaluated
         }
-        //Esperar a que las hebras creadas terminen.
+        // Esperar a que las hebras creadas terminen.
     }
 
-    public static String solve(String input, Map<Character, Function> functions) {
+    public static Float solve(String input, Map<Character, Function> functions) {
         MutableString solution = new MutableString(input);
         List<String> toResolve = Function.getFunctions(input);
 
         Function functionToSolve;
         String functionEvaluated;
         String argument;
-        for (String function: toResolve) {
+        for (String function : toResolve) {
             functionToSolve = functions.get(function.charAt(0));
-            argument = function.substring(2, function.length()-1);
+            argument = function.substring(2, function.length() - 1);
             functionEvaluated = functionToSolve.equation.replaceAll(functionToSolve.parameter.toString(), argument);
 
             solution.replaceAll(function, '(' + functionEvaluated + ')', true);
-            //Crear una nueva hebra con input = function Evaluated
+            // Crear una nueva hebra con input = function Evaluated
         }
-        //Esperar a que las hebras creadas terminen.
-        return solution.toString();
+        // Esperar a que las hebras creadas terminen.
+        try {
+            Equation result = new Equation(solution.toString());
+            return result.solve();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
