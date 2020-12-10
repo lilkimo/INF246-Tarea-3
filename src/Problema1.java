d2418c1a5647f0a70ebf17af67f770a7aeb94a11
lilkimo <zarkokuljis.zk@gmail.com>
2020-12-10 18:25:56 -0300

    fixed issues

--- src/Problema1.java
+++ src/Problema1.java
@@ -23,12 +23,17 @@ class Problema1 {
                 throw new Exception("Function \"" + line + "\" cannot be parsed");
             }
         }
+        //Mostrar el diccionario
         /*
         for (Map.Entry<Character, Function> entry : functions.entrySet())
             System.out.println(entry.getKey() + ": " + entry.getValue().toString());
         */
+        
         Solver.solve("f(1+2+3)+g(4)", functions);
+        //System.out.println(Function.getFunctions("f(1+2+3)+g(4)"));
+        //System.out.println(Function.getFunctions("g(1)*h(1)+1054"));
 
+        //Ejemplo de uso de un MutableString
         /*
         MutableString a = new MutableString("f(x)+4");
         MutableString b = a;
@@ -36,6 +41,7 @@ class Problema1 {
         System.out.println(a);
         System.out.println(b);
         */
+
         //Lee el input
         /*
         Scanner lector = new Scanner(System.in);
